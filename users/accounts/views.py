c00635c8d0cf20e5629b7b5b28e7312a7296d994
Marcelo Schneider <schneider.fei@gmail.com>
2020-08-26 15:48:01 -0300

    Adiciona view para login - issue #1
    Adiciona view para login, esta view faz a requisição do request token e
    depois redireciona o usuário para o endereço de login do service provider.

--- users/accounts/views.py
+++ users/accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import RedirectView
+from django.conf import settings
 
-# Create your views here.
+
+class OAuthLogin(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        return self.redirect_uri.url
+
+    def setup(self, request, *args, **kwargs):
+        oauth_usp = getattr(settings, 'OAUTH_USP')
+        usp = oauth_usp.create_client('usp')
+        redirect_uri = getattr(settings, 'REDIRECT_URI')
+        self.redirect_uri = usp.authorize_redirect(request, redirect_uri)
+        return super().setup(request, *args, **kwargs)
+
+
+accounts_login = OAuthLogin.as_view()
