51d980daf1f20b504fda5c763146af4318976eb0
Marcelo Schneider <schneider.fei@gmail.com>
2020-08-26 15:40:05 -0300

    Testa views login - issue #1

---
+++ users/accounts/tests/test_accounts_views.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from django.shortcuts import resolve_url as r
+
+class LoginViewsTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get(r('accounts:login'))
+
+    def test_status_code(self):
+        """Status code should be 302"""
+        self.assertEqual(302, self.resp.status_code)
