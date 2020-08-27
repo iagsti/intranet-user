929b55fadefe4ea4b9cbbe30d6687b6a966548c1
Marcelo Schneider <schneider.fei@gmail.com>
2020-08-27 14:49:38 -0300

    Testa autorização do oauth_token - issue #1

--- users/accounts/tests/test_oauth.py
+++ users/accounts/tests/test_oauth.py
@@ -1,15 +1,28 @@
+import json
 from django.test import TestCase
-from authlib.integrations.django_client import OAuth
+from authlib.integrations.django_client.integration import DjangoRemoteApp
+from django.http import HttpRequest, QueryDict
+from django.conf import settings
 
-from users.accounts.oauth import OAuthRequestToken
+from users.accounts.oauth import OAuthUsp
+from .mock import mock_oauth, token, resource, verifier
 
 
-class OAuthRequestTokenTest(TestCase):
+class OAuthUspTest(TestCase):
     def setUp(self):
-        self.obj = OAuthRequestToken()
+        self.obj = OAuthUsp()
 
     def test_has_attribute_oauth_usp(self):
         self.assertTrue(hasattr(self.obj, 'oauth_usp'))
 
     def test_oauth_usp_instance(self):
-        self.assertIsInstance(self.obj.oauth_usp, OAuth)
+        self.assertIsInstance(self.obj.oauth_usp, DjangoRemoteApp)
+
+    @mock_oauth
+    def test_get_authorize_redirect(self):
+        request = HttpRequest
+        setattr(request, 'session', {})
+        resp = self.obj.get_authorize_redirect(request)
+        expected = 'https://uspdigital.usp.br/wsusuario/oauth/authorize'
+        self.assertIn(expected, resp.url)
+
