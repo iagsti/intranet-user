84fc5f678762d78ee196b4544d3aeef4e7df90a8
Marcelo Schneider <schneider.fei@gmail.com>
2020-08-27 15:17:10 -0300

    Adiciona rotas para login e authorize - issue #1

--- users/accounts/urls.py
+++ users/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import accounts_login
+from .views import accounts_login, accounts_authorize
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login', accounts_login, name='login')
+    path('login', accounts_login, name='login'),
+    path('authorize', accounts_authorize, name='authorize'),
 ]
