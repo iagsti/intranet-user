36c51a0bbf96f195634393f9dcfe87410835665d
Marcelo Schneider <schneider.fei@gmail.com>
2020-09-23 17:46:45 -0300

    Adiciona nome ao atributo upstream - issue #11

--- users/proxy/models.py
+++ users/proxy/models.py
@@ -4,4 +4,4 @@ from django.utils.translation import gettext as _
 
 class Upstreams(models.Model):
     path = models.CharField(_("path"), max_length=50)
-    upstream = models.URLField(_(""), max_length=200)
+    upstream = models.URLField(_("upstream"), max_length=200)
